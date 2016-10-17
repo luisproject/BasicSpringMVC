7be6e2f752144edba9306e896c879644544da2bc
Nguyen Van Vuong <vuongluis@hotmail.com>
2016-10-17 16:18:51 +0700

    ok

---
+++ SpringMVC/LearnSpringMVC/src/main/java/vuongluis/dut/udn/vn/springmvc/App.java
@@ -0,0 +1,13 @@
+package vuongluis.dut.udn.vn.springmvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
